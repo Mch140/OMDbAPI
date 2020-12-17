080dd5743625622f7c3998caa02dc6b2508d84d9
Mch140 <75289572+Mch140@users.noreply.github.com>
2020-12-17 09:26:48 +0530

    Update sdkMethods.js

--- docs/sdkMethods.js
+++ docs/sdkMethods.js
@@ -1,18 +1,20 @@
 console.log(">>>>>>>>>> inside sdkMethod.js");
 errorMessage = document.getElementById("errorMessage");
 // chatText = "chatTranscript.lines";
-console.log(>>>>>>>>>> chatText);
+console.log(">>>>>>>>>> chatText: "+ chatText);
+chatText = "chatTranscript.lines";
 
  
 
 var updateCallback = function(data){
+    console.log(">>>>>>>>>> data in updateCallback: "+ data);
+    var path = data.key;
+    console.log(">>>>>>>>>> data.key in updateCallback: "+ path);
     var value = data.newValue;
-    console.log("after value ");
-    console.log("value : "+value);
+    console.log(">>>>>>>>>> data.newValue in updateCallback: "+ value);
     var line = value[value.length -1];
-    console.log("line : "+line);
+    console.log(">>>>>>>>>> newValue[-1] in updateCallback: "+ line);
     var movieName = line.text;
-    console.log("after movieName");
     console.log("moviename : "+movieName);
     if (line.source.toLowerCase()==="visitor"){
         var url = "https://www.omdbapi.com?t="+movieName+"&apikey=fb7c45ff";
@@ -25,11 +27,14 @@ var updateCallback = function(data){
                     document.getElementById("Title").innerHTML = res.Title;
                     document.getElementById("Year").innerHTML = res.Year;
                     document.getElementById("Rated").innerHTML = res.Rated;
-                    document.getElementById("Actors").innerHTML = res.Actors;
+                    document.getElementById("Released").innerHTML = res.Released;
+                    document.getElementById("Runtime").innerHTML = res.Runtime;
                     document.getElementById("Genre").innerHTML = res.Genre;
+                    document.getElementById("Director").innerHTML = res.Director;
+                    document.getElementById("Actors").innerHTML = res.Actors;
                     document.getElementById("Plot").innerHTML = res.Plot;
-
- 
+                    document.getElementById("Awards").innerHTML = res.Awards;
+                    document.getElementById("imdbRating").innerHTML = res.imdbRating;
 
                 }).catch(function(error){
                     console.log("I am inside catch");
